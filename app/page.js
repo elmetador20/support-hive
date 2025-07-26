fb68b0d21516e6066afb8b4b81cd21d7e35853f9
sharique ahmad <ahmedsharique250@gmail.com>
2025-07-27 00:12:15 +0530

    minor change

--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
   <div className="font-extrabold flex md:gap-5 gap-2 md:text-5xl mb-2 text-3xl">Buy Me a Chai<span><img className="invertImg" src="/tea1.gif"width={58} alt="sorry"/></span></div>
   
   <p className="text-sm md:text-base text-centre md:text-left max-w-xl text-gray-300 mb-6 text-center">
-    A crowd-funding platform to support your projects with chai. Get funded by your fans and followers.
+    A crowd-funding platform to support your projects with chai.
   </p>
-  
+  <p className="text-sm md:text-base text-centre md:text-left max-w-xl text-gray-300 mb-6 text-center"> Get funded by your fans and followers.</p>
   <div>
     <Link href="/login">
     <button className="bg-transparent border-2 border-white text-white font-semibold px-6 py-2 rounded-md ml-4 hover:bg-white hover:text-[#121f4a] transition duration-200">
@@ -32,17 +32,17 @@ export default function Home() {
         Your fans can buy you a chai</h1>
       
       <div className="  flex gap-5 justify-around ">
-         <div className="item space-y-3 flex flex-col items-center justify-centre rounded-full">
+         <div className="  text-white item space-y-3 flex flex-col items-center justify-centre rounded-full">
           <img width={100} src="/chai1.gif" alt="Chai 1" className="w-24 h-24 mx-auto mb-2 p-2" />
           <p className=" font-bold text-center text-white-800"> Fans want to help</p>
           <p className=" text-center">Your fans are available for you to help you</p>
         </div>
-         <div className="item space-y-3 flex flex-col items-center justify-centre rounded-full">
+         <div className="  text-white item space-y-3 flex flex-col items-center justify-centre rounded-full">
           <img width={100} src="/coin.gif" alt="Chai 1" className="w-24 h-24 mx-auto mb-2 p-2" />
           <p className=" font-bold text-center text-white-800">Fans want to help</p>
           <p className=" text-center">Your fans are available for you to help you</p>
         </div>
-        <div className="item space-y-3 flex flex-col items-center justify-centre rounded-full">
+        <div className="  text-white item space-y-3 flex flex-col items-center justify-centre rounded-full">
           <img width={50} src="/fans.gif" alt="Chai 1" className="w-24 h-24 mx-auto mb-2 p-2" />
           <p className=" font-bold text-center text-white-800">Fans want to help</p>
           <p className=" text-center">Your fans are available for you to help you</p>
