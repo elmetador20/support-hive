9e194da2390eb01be35324d6c2ef201b90c524be
sharique ahmad <ahmedsharique250@gmail.com>
2025-07-27 02:32:24 +0530

    changes

--- app/components/PaymentPage.js
+++ app/components/PaymentPage.js
@@ -41,7 +41,7 @@ const PaymentPage=({ username })=> {
       "description": "Test Transaction",
       "image": "https://example.com/your_logo",
       "order_id": orderID, // This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-      "callback_url": `${NEXT_PUBLIC_URL}/api/razorpay`,
+      "callback_url": `https://support-hive-mu.vercel.app/api/razorpay`,
       "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
         "name": "Gaurav Kumar", //your customer's name
         "email": "gaurav.kumar@example.com",
